94268bd8cf008d96b4c1a5e84ef25fe2cf780af9
petrichor <moujdid53@gmail.com>
2024-05-03 13:44:50 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ClassController;
+use App\Http\Middleware\AdminMiddleware ;
+use App\Http\Middleware\TeacherMiddleware;
+use App\Http\Middleware\StudentMiddleware;
+use Bootstrap\App;
+
+
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+
+Route::get('/', [AuthController::class, 'Login']);
+Route::post('login', [AuthController::class, 'AuthLogin']);
+Route::get('logout', [AuthController::class, 'logout']);
+
+Route::get('forgot-password', [AuthController::class, 'forgotpassword']);
+Route::post('forgot-password', [AuthController::class, 'PostForgotPassword']);
+
+Route::get('reset/{token}', [AuthController::class, 'reset']);
+Route::post('reset/{token}', [AuthController::class, 'PostReset']);
+
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+    Route::get('admin/admin/list', [AdminController::class, 'list']);
+    Route::get('admin/admin/add', [AdminController::class, 'add']);
+    Route::post('admin/admin/add', [AdminController::class, 'insert']);
+    Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);
+    Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
+    Route::get('admin/admin/delete/{id}', [AdminController::class, 'delete']);
+
+    //class url
+
+    Route::get('admin/class/list', [ClassController::class, 'list']);
+    Route::get('admin/class/add', [ClassController::class, 'add']);
+    Route::post('admin/class/add', [ClassController::class, 'insert']);
+    Route::get('admin/class/edit/{id}', [ClassController::class, 'edit']);
+    Route::post('admin/class/edit/{id}', [ClassController::class, 'update']);
+    Route::get('admin/class/delete/{id}', [ClassController::class, 'delete']);
+
+
+});
+
+Route::group(['middleware' => 'teacher'], function () {
+    Route::get('teacher/dashboard', [DashboardController::class, 'dashboard']);
+});
+
+Route::group(['middleware' => 'student'], function () {
+    Route::get('student/dashboard', [DashboardController::class, 'dashboard']);
+});
