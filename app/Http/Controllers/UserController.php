2085630d669bfd55067fc443c1ed72f22bd77161
achraf <chalkhaachraf03@gmail.com>
2024-05-06 22:28:53 +0100

    Admin edit profil and student edit profil are done (still teacher edit profil not yet)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,81 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
 use Hash;
+use Str;
 
 class UserController extends Controller
-{
+{   
+    public function MyAccount(){
+        $data['getRecord'] = User::getSingle(Auth::user()->id);
+        $data['header_title'] = 'My Account ';
+        if(Auth::user()->user_type == 1)
+        {
+            return view('admin.my_account',$data);
+        }
+        else if(Auth::user()->user_type == 2)
+        {
+            return view('teacher.my_account',$data);
+        }
+        else if(Auth::user()->user_type == 3)
+        {
+            return view('student.my_account',$data);
+
+        }
+
+    }
+    public function UpdateMyAccountAdmin(Request $request){
+        $id = Auth::user()->id;
+        request()->validate([
+            'email' =>'required|email|unique:users,email,'.$id,
+           
+            
+        ]);
+        $admin = User::getSingle($id);
+        $admin->name = trim($request->name);
+        $admin->email = trim($request->email);
+        $admin->save();
+        return redirect('admin/account')->with('success',"Account successfully updated");
+
+    }
+    public function UpdateMyAccountStudent(Request $request)
+    {
+        $id = Auth::user()->id;
+        request()->validate([
+            'email' =>'required|email|unique:users,email,'.$id,
+            'mobile_number'=>'max:15|min:10',
+            
+        ]);
+      
+        $student = User::getSingle($id);
+        $student->name = trim($request->name);
+        $student->last_name = trim($request->last_name);
+        $student->gender = trim($request->gender);
+        if(!empty($request->date_of_birth)){
+            $student->date_of_birth = trim($request->date_of_birth);
+
+        }
+        if(!empty($request->file('profile_pic'))) {
+              if(!empty($student->getProfile()))
+              {
+                unlink('upload/profile/'.$student->profile_pic);
+              }
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/',$filename);
+            $student->profile_pic = $filename;
+        }    
+        
+          $student->mobile_number = trim($request->mobile_number);
+          $student->email = trim($request->email);
+       
+        
+        
+        $student->save();
+        return redirect('student/account')->with('success',"Account successfully updated");
+
+    }
     public function change_password(){
         $data['header_title'] = 'Change Password';
         return view('profile.change_password',$data);
--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,11 @@ Route::get('admin/dashboard', function () {
     Route::get('admin/assign_subject/edit_single/{id}', [ClassSubjectController::class, 'edit_single']);
     Route::post('admin/assign_subject/edit_single/{id}', [ClassSubjectController::class, 'update_single']);
 
+
+    Route::get('admin/account', [UserController::class, 'MyAccount']);
+    Route::post('admin/account', [UserController::class, 'UpdateMyAccountAdmin']);
+     
+
     //edit password url
     
     Route::get('admin/change_password', [UserController::class, 'change_password']);
@@ -113,7 +118,9 @@ Route::group(['middleware' => 'teacher'], function () {
 
 Route::group(['middleware' => 'student'], function () {
     Route::get('student/dashboard', [DashboardController::class, 'dashboard']);
-
+    Route::get('student/account', [UserController::class, 'MyAccount']);
+    Route::post('student/account', [UserController::class, 'UpdateMyAccountStudent']);
+     
     Route::get('student/change_password', [UserController::class, 'change_password']);
     Route::post('student/change_password', [UserController::class, 'update_change_password']);
 });
