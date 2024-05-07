23406ed3f383f35c7d996ad332eae098f50033dd
achraf <chalkhaachraf03@gmail.com>
2024-05-07 20:09:01 +0100

    student side show subjects doneee

--- resources/views/admin/my_account.blade.php
+++ resources/views/admin/my_account.blade.php
@@ -18,23 +18,76 @@
         <div class="row">
           <!-- left column -->
           <div class="col-md-12">
-            @include('_messages');
+            @include('_messages')
 
             <!-- general form elements -->
             <div class="card card-primary">
               <form method="POST" action="">
                 {{  csrf_field() }}
                 <div class="card-body">
-                  <div class="form-group">
-                    <label >Name</label>
-                    <input type="text" class="form-control" name="name" value="{{ old('name',$getRecord->name) }}" required placeholder="Name">
+                  <div class="row">  
+                  <div class="form-group col-md-6">
+                    <label >First Name</label>
+                    <input type="text" class="form-control" name="name" value="{{ old('name',$getRecord->name) }}" required placeholder="First Name">
                   </div>
-                  <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" class="form-control" name="email" value="{{ old('email',$getRecord->email) }}" required placeholder="Email">
-                    <div style="color:red">{{ $errors->first('email') }}</div>
+                  <div class="form-group col-md-6">
+                    <label>Last Name</label>
+                    <input type="text" class="form-control" name="last_name" value="{{ old('last_name',$getRecord->last_name) }}" required placeholder="Last Name">
+                    <div style="color:red">{{ $errors->first('last_name') }}</div>
                   </div>
                   
+                  </div>
+                  
+                  <div class="row">
+                       
+                    <div class="form-group col-md-6">
+                      <label >Gender  <span style="color: red;"></span></label>
+                       <select class="form-control"  required name="gender">
+                          <option  value="">Select Gender</option>
+                          <option {{(old('gender',$getRecord->gender)=='Male') ? 'selected' : ''}} value="Male">Male</option>
+                          <option {{(old('gender',$getRecord->gender)=='Female') ? 'selected' : ''}} value="Female">Female</option>
+
+                       </select>
+                       <div style="color:red">{{ $errors->first('gender') }}</div>
+
+                       
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label >Mobile Number <span style="color: red;"></span></label>
+                      <input type="text" class="form-control"  value="{{ old('mobile_number',$getRecord->mobile_number) }}" name="mobile_number"  placeholder="mobile_number">
+                      <div style="color:red">{{ $errors->first('mobile_number') }}</div>
+
+                    </div>
+                    
+              </div>  
+              <div class="row">
+                <div class="form-group col-md-6">
+                    <label >Date of birth <span style="color: red;"></span></label>
+                    <input type="date" class="form-control" required value="{{ old('date_of_birth',$getRecord->date_of_birth) }}" name="date_of_birth" required placeholder="date of birth">
+                    <div style="color:red">{{ $errors->first('date_of_birth') }}</div>
+
+                    
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label >Profile Pic <span style="color: red;"></span></label>
+                    <input type="file" class="form-control"  name="profile_pic">
+                    <div style="color:red">{{ $errors->first('profile_pic') }}</div>
+                    @if(!empty($getRecord->getProfile()))
+                      <img src="{{$getRecord->getProfile()}}" style="width: auto; height: 50px;">
+                    @endif  
+
+                  </div>  
+                 
+            </div>   
+                  <div class="row">  
+                    
+                    <div class="form-group col-md-6">
+                      <label>Email</label>
+                      <input type="email" class="form-control" name="email" value="{{ old('email',$getRecord->email) }}" required placeholder="Email">
+                      <div style="color:red">{{ $errors->first('email') }}</div>
+                    </div>
+                    
+                    </div>
                 </div>
                 <div class="card-footer">
                   <button type="submit" class="btn btn-primary">Update</button>
