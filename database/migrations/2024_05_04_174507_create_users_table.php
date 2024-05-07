fdcbbe1632ceb67957fc56be59de867aba821180
petrichor <moujdid53@gmail.com>
2024-05-07 15:20:47 +0100

    lidl database tweaking

--- database/migrations/2024_05_04_174507_create_users_table.php
+++ database/migrations/2024_05_04_174507_create_users_table.php
@@ -20,19 +20,16 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->tinyInteger('user_type')->default(3)->comment('1:admin, 2:teacher,3;student');
-            $table->integer('is_deleted')->default(0)->comment('0:not deleted,1:deleted');
-           
-            
-$table->string('CIN')->nullable();
-$table->string('CNE')->nullable();
-$table->integer('class_id')->nullable();
-
-$table->string('gender')->nullable();
-$table->date('date_of_birth')->nullable();
-$table->string('profile_pic')->nullable();
-$table->date('admission_date')->nullable();
-$table->string('mobile_number')->unique()->nullable();
-$table->string('status');
+            $table->integer('is_deleted')->default(0)->comment('0:not deleted,1:deleted');          
+            $table->string('CIN')->nullable();
+            $table->string('CNE')->nullable();
+            $table->integer('class_id')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('profile_pic')->nullable();
+            $table->date('admission_date')->nullable();
+            $table->string('mobile_number')->unique()->nullable();
+            $table->string('status');
             $table->timestamps();
         });
     }
